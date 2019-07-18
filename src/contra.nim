dce743d4da1c6cc3cd7ca4dc7730829e3f6d57d1
Juan Carlos <juancarlospaco@gmail.com>
2019-07-18 16:56:45 -0300

    Add Optional Security Hardened mode (Based from Debian Hardened & Gentoo Hardened)

--- src/contra.nim
+++ src/contra.nim
@@ -1,7 +1,8 @@
 ## Contra
 ## ======
 ##
-## Lightweight Contract Programming(DbC),designed for ``proc``/``func``,
+## Lightweight Contract Programming (DbC) and optional Hardened Build mode
+## (Based from Debian Hardened & Gentoo Hardened),designed for ``proc``/``func``,
 ## 2 Templates for Preconditions (Require) & Postconditions (Ensure) on 9 LoC.
 ## For performance it wont generate any code when build for release. Recommended
 ## `func <https://nim-lang.org/docs/manual.html#procedures-func>`_ and use of
@@ -22,9 +23,23 @@ template postconditions*(ensures: varargs[bool]) =
       for i, contractPostcondition in ensures: assert(contractPostcondition,
           "\nContract Postcondition (Ensure) failed assert on position: " & $i)
 
+template hardenedBuild*() =
+  ## Optional Security Hardened mode (Based from Debian Hardened & Gentoo Hardened).
+  ## See: http:wiki.debian.org/Hardening & http:wiki.gentoo.org/wiki/Hardened_Gentoo
+  ## http:security.stackexchange.com/questions/24444/what-is-the-most-hardened-set-of-options-for-gcc-compiling-c-c
+  # Hardened build is ideal companion for a Contracts module,still optional anyway.
+  when defined(hardened) and defined(gcc) and not defined(objc) and not defined(js) and not defined(cpp):
+    when defined(danger): {.fatal: "-d:hardened is incompatible with -d:danger".}
+    when not defined(contracts): {.fatal: "-d:hardened requires -d:contracts".}
+    {.hint: "Security Hardened mode is enabled. Running Hardened.".}
+    const hf = "-fstack-protector-all -Wstack-protector --param ssp-buffer-size=4 -pie -fPIE -Wformat -Wformat-security -D_FORTIFY_SOURCE=2 -Wall -Wextra -Wconversion -Wsign-conversion -mindirect-branch=thunk -mfunction-return=thunk -fstack-clash-protection -Wl,-z,relro,-z,now -Wl,-z,noexecstack -fsanitize=signed-integer-overflow -fsanitize-undefined-trap-on-error -Wl,-z,noexecheap -fno-common"
+    {.passC: hf, passL: hf.}
+
 
 # when isMainModule:
 runnableExamples:
+  hardenedBuild() ## Security Hardened mode enabled, compile with:  -d:hardened
+
   func funcWithContract(mustBePositive: int): int {.compiletime.} =
     preconditions mustBePositive > 0, mustBePositive > -1 ## Require (Preconditions)
     postconditions result > 0, result < int32.high        ## Ensure  (Postconditions)
