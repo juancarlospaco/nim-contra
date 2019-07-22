f0a91cdfbb4577a5788cf27fb14a5d35ce49676f
Juan Carlos <juancarlospaco@gmail.com>
2019-07-22 11:25:33 -0300

    Cleanout

--- src/contra.nim
+++ src/contra.nim
@@ -28,20 +28,20 @@ template hardenedBuild*() =
   ## See: http:wiki.debian.org/Hardening & http:wiki.gentoo.org/wiki/Hardened_Gentoo
   ## http:security.stackexchange.com/questions/24444/what-is-the-most-hardened-set-of-options-for-gcc-compiling-c-c
   # Hardened build is ideal companion for a Contracts module,still optional anyway.
-  when defined(hardened) and defined(gcc) and not defined(objc) and not defined(js) and not defined(cpp):
+  when defined(hardened) and defined(gcc) and not defined(objc) and not defined(js):
     when defined(danger):
       {.fatal: "-d:hardened is incompatible with -d:danger".}
     when not defined(contracts):
       {.fatal: "-d:hardened requires -d:contracts".}
     {.hint: "Security Hardened mode is enabled.".}
-    const hf = "-fstack-protector-all -Wstack-protector --param ssp-buffer-size=4 -pie -fPIE -Wformat -Wformat-security -D_FORTIFY_SOURCE=2 -Wall -Wextra -Wconversion -Wsign-conversion -mindirect-branch=thunk -mfunction-return=thunk -fstack-clash-protection -Wl,-z,relro,-z,now -Wl,-z,noexecstack -fsanitize=signed-integer-overflow -fsanitize-undefined-trap-on-error -Wl,-z,noexecheap -fno-common"
+    const hf = "-fstack-protector-all -Wstack-protector --param ssp-buffer-size=4 -pie -fPIE -Wformat -Wformat-security -D_FORTIFY_SOURCE=2 -Wall -Wextra -Wconversion -Wsign-conversion -mindirect-branch=thunk -mfunction-return=thunk -fstack-clash-protection -Wl,-z,relro,-z,now -Wl,-z,noexecstack -fsanitize=signed-integer-overflow -fsanitize-undefined-trap-on-error -fno-common"
     {.passC: hf, passL: hf.}
 
-  when defined(danger) and defined(gcc) and not defined(hardened) and not defined(objc) and not defined(js):
-    {.passC: "-flto -ffast-math -march=native -mtune=native".}
+  when defined(danger) and defined(gcc) and not defined(hardened):
+    {.passC: "-flto -ffast-math -march=native -mtune=native -fsingle-precision-constant".}
 
-  when defined(release):
-    {.passL: "-s".}
+  when defined(release) and defined(gcc):
+    {.passL: "-s", passC: "-fno-ident".}
 
 
 # when isMainModule:
