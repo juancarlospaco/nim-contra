a6b9901b74003a7002ebca122ffe4e1543d1dcbd
Juan Carlos <juancarlospaco@gmail.com>
2019-07-23 00:35:44 -0300

    Improve Hardened

--- src/contra.nim
+++ src/contra.nim
@@ -35,7 +35,7 @@ template hardenedBuild*() =
       {.fatal: "-d:hardened requires -d:contracts".}
     {.hint: "Security Hardened mode is enabled.".}
     const hf = "-fstack-protector-all -Wstack-protector --param ssp-buffer-size=4 -pie -fPIE -Wformat -Wformat-security -D_FORTIFY_SOURCE=2 -Wall -Wextra -Wconversion -Wsign-conversion -mindirect-branch=thunk -mfunction-return=thunk -fstack-clash-protection -Wl,-z,relro,-z,now -Wl,-z,noexecstack -fsanitize=signed-integer-overflow -fsanitize-undefined-trap-on-error -fno-common"
-    {.passC: hf, passL: hf.}
+    {.passC: hf, passL: hf, assertions: on, checks: on.}
 
   when defined(danger) and defined(gcc) and not defined(hardened):
     {.passC: "-flto -ffast-math -march=native -mtune=native -fsingle-precision-constant".}
@@ -44,8 +44,8 @@ template hardenedBuild*() =
     {.passL: "-s", passC: "-fno-ident".}
 
 
-# when isMainModule:
-runnableExamples:
+when isMainModule:
+  ##runnableExamples:
   hardenedBuild() ## Security Hardened mode enabled, compile with:  -d:hardened
 
   func funcWithContract(mustBePositive: int): int {.compiletime.} =
